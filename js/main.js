6ace82fdd84674326e5af5dc5c3afe072d95f431
Icia <icia83@gmail.com>
2018-03-28 18:22:44 +0200

    add menu styles and background, create open/close function

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Variables
+var page = document.querySelector('.page');
+var btnOpenMenu = page.querySelector('.nav-trigger');
+var btnClose = page.querySelector('.nav-close');
+
+// Functions
+function openMenu() {
+	page.classList.add('page--nav-visible');
+};
+
+
+function closeMenu() {
+	page.classList.remove('page--nav-visible');
+};
+
+//Events
+btnOpenMenu.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
